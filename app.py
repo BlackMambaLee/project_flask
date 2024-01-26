da97e9100141172346fc61b70844083dc4010570
BlackMambaLee <grossome@naver.com>
2024-01-26 19:56:54 +0900

    변경

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ def return_call():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='5001')
+    app.run(debug=True, host='0.0.0.0', port='5000')
     # app.run(debug=True, host='127.0.0.1')
